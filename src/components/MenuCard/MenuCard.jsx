f9eea54e34c58be6f911b4870ba616f17181cbdd
Your Name <itzalbert2@gmail.com>
2019-08-08 10:38:10 -0700

    Break out MenuCard logic into a separate pure component

--- src/components/MenuCard/MenuCard.jsx
+++ src/components/MenuCard/MenuCard.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
 
-// renderList(foods) {
-//   var list = [];
-//   foods.forEach(v => {
-//     list.push(
-//       <div style={{ marginBottom: '40px', display: 'flex', flexDirection: 'column' }}>
-//         <div>name: {v.name}</div>
-//         <div>brand: {v.brand}</div>
-//         <div>calories: {v.calories}</div>
-//         <div>portion: {v.portion}</div>
-//       </div>
-//     );
-//   });
-//   return (
-//     <div>
-//       <h3>Result:</h3>
-//       {list}
-//     </div>
-//   );
-// }
+const FoodCards = props => {
+  const list = [];
+  const { foods } = props;
+
+  foods.forEach(v => {
+    list.push(
+      <div style={{ marginBottom: '40px', display: 'flex', flexDirection: 'column' }}>
+        <div>name: {v.name}</div>
+        <div>brand: {v.brand}</div>
+        <div>calories: {v.calories}</div>
+        <div>portion: {v.portion}</div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h3> Result:</h3>
+      {list}
+    </div>
+  );
+};
+
+export default FoodCards;
