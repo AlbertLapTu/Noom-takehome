751cf7698e7e8705ff4a1ac4a0bb8deefe007701
Your Name <itzalbert2@gmail.com>
2019-08-08 11:15:13 -0700

    Complete Requirement 2 by using debounce function

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import MenuCard from '../MenuCard/MenuCard';
+import HeaderImage from './assets/image.png';
+import debounce from '../../utilities/utilityFunctions';
 
 export default class CardContainer extends React.Component {
   constructor() {
     super();
     this.state = {
       foods: [],
-      searchQuery: ''
+      searchQuery: null
     };
   }
 
-  handleChange = event => {
-    this.setState(
-      {
-        [event.target.name]: event.target.value
-      },
-      () => {
-        const { searchQuery } = this.state;
-        if (searchQuery.length < 3) {
-          return null;
-        } else {
-          fetch(
-            `https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search?kv=${searchQuery}`
-          )
-            .then(res => res.json())
-            .then(data => {
-              this.setState({ foods: data });
-            });
-        }
-      }
-    );
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleKeyDown = () => {
+    const { searchQuery } = this.state;
+
+    if (searchQuery.length < 3) {
+      return;
+    } else {
+      fetch(`https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search?kv=${searchQuery}`)
+        .then(res => res.json())
+        .then(data => {
+          this.setState({ foods: data });
+        });
+    }
   };
 
   render() {
@@ -37,20 +37,21 @@ export default class CardContainer extends React.Component {
 
     return (
       <div className='App'>
+        <img src={HeaderImage} />
         <h1>Noom Food Explorer</h1>
-        <form onSubmit={this.handleSubmit}>
+        <form>
           <label>
             Enter the key word:
             <input
               type='text'
               name='searchQuery'
-              value={this.state.value}
               onChange={this.handleChange}
+              onKeyDown={debounce(this.handleKeyDown, 200)}
             />
           </label>
           <button type='submit'>Search</button>
         </form>
-        <MenuCard foods={foods} />
+        {foods.length === 0 ? null : <MenuCard foods={foods} />}
       </div>
     );
   }
