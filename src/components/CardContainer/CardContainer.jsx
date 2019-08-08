b8910fbc511de4c325778f1a3dce3b593e6f2344
Your Name <itzalbert2@gmail.com>
2019-08-08 10:37:17 -0700

    Complete Requirement 1. Results displaying on user change event

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-
-/*
-Card container should initiate the API request on search, and render the Menu card components
-
-
-
-*/
+import MenuCard from '../MenuCard/MenuCard';
 
 export default class CardContainer extends React.Component {
   constructor() {
@@ -16,20 +10,31 @@ export default class CardContainer extends React.Component {
     };
   }
 
-  handleSubmit = event => {
-    event.preventDefault();
-    fetch('https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search?kv=chicken')
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ foods: data });
-      });
-  };
-
   handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState(
+      {
+        [event.target.name]: event.target.value
+      },
+      () => {
+        const { searchQuery } = this.state;
+        if (searchQuery.length < 3) {
+          return null;
+        } else {
+          fetch(
+            `https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search?kv=${searchQuery}`
+          )
+            .then(res => res.json())
+            .then(data => {
+              this.setState({ foods: data });
+            });
+        }
+      }
+    );
   };
 
   render() {
+    const { foods } = this.state;
+
     return (
       <div className='App'>
         <h1>Noom Food Explorer</h1>
@@ -43,11 +48,9 @@ export default class CardContainer extends React.Component {
               onChange={this.handleChange}
             />
           </label>
-          <button type='submit' onSubmit={this.handleSubmit}>
-            Search
-          </button>
+          <button type='submit'>Search</button>
         </form>
-        {/* {this.renderList(this.state.foods)} */}
+        <MenuCard foods={foods} />
       </div>
     );
   }
