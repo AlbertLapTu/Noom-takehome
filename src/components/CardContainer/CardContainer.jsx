29f9716555b48d7bff24dab9feb76c0da112883d
Your Name <itzalbert2@gmail.com>
2019-08-13 13:06:51 -0700

    Partially build out logic for infinite scroll. fetchPage function needs additional tweaking to account for page value offsets

--- src/components/CardContainer/CardContainer.jsx
+++ src/components/CardContainer/CardContainer.jsx
@@ -9,9 +9,57 @@ export default class CardContainer extends React.Component {
     super();
     this.state = {
       foods: [],
-      searchQuery: null
+      searchQuery: null,
+      maxWindowHeight: 0,
+      currentPage: 1
     };
   }
+
+  componentDidMount = () => {
+    window.addEventListener('scroll', this.handleScrollLocation);
+  };
+
+  getDocHeight = () => {
+    var D = document;
+    return Math.max(
+      D.body.scrollHeight,
+      D.documentElement.scrollHeight,
+      D.body.offsetHeight,
+      D.documentElement.offsetHeight,
+      D.body.clientHeight,
+      D.documentElement.clientHeight
+    );
+  };
+
+  handleFetchPage = () => {
+    const { page, foods, currentPage, searchQuery } = this.state;
+
+    fetch(
+      `https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search?kv=${searchQuery}$pg=${page}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          foods: [...foods, ...data],
+          currentPage: currentPage + 1
+        });
+      });
+  };
+
+  handleScrollLocation = () => {
+    //TODO: Window height is at: 12494, scrollY when at end of page is 11673.
+    //Run the fetchPage function after scrollY goes past a certain threshold.
+
+    const { maxWindowHeight } = this.state;
+    console.log(maxWindowHeight, 'window Height');
+    console.log(window.scrollY, 'scrollY');
+
+    //Checks to see if we should fetch new data
+    if (window.scrollY === maxWindowHeight) {
+      //Fetch second page
+      this.handleFetchPage();
+    }
+  };
 
   handleChange = e => {
     this.setState({
@@ -22,24 +70,24 @@ export default class CardContainer extends React.Component {
   handleKeyDown = () => {
     const { searchQuery } = this.state;
 
+    //append param pg=#
     if (searchQuery.length < 3) {
       return;
     } else {
       fetch(`https://uih0b7slze.execute-api.us-east-1.amazonaws.com/dev/search?kv=${searchQuery}`)
         .then(res => res.json())
         .then(data => {
-          this.setState({ foods: data });
+          this.setState({ foods: data, maxWindowHeight: this.getDocHeight() });
         });
     }
   };
 
   render() {
     const { foods } = this.state;
-
     return (
       <div className='App'>
         <div className='header-container'>
-          <img src={HeaderImage} />
+          <img src={HeaderImage} alt='header' />
           <div className='header-content'>
             <h1>Noom Food Explorer</h1>
             <form>
@@ -51,6 +99,7 @@ export default class CardContainer extends React.Component {
                 name='searchQuery'
                 onChange={this.handleChange}
                 onKeyDown={debounce(this.handleKeyDown, 200)}
+                onScroll={this.handleOnScroll}
               />
               <button className='search-btn' type='submit'>
                 Search
